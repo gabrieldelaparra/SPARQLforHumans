40c6a300018a66857f7eb1b23c09a339b0f1ec70
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-05-25 18:03:39 -0400

    Refactor to RDF Extensions

---
+++ SparqlForHumans.Core/Services/RDFExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace SparqlForHumans.Core.Services
+{
+    public static class RDFExtensions
+    {
+        public static Triple GetTriple(this string inputLine)
+        {
+            var g = new NonIndexedGraph();
+            StringParser.Parse(g, inputLine);
+            return g.Triples.Last();
+        }
+
+        public static bool IsUriNode(this INode node)
+        {
+            return node.NodeType == NodeType.Uri;
+        }
+
+        public static bool IsValidSubject(this INode node)
+        {
+            return node.ToSafeString().Contains(Properties.WikidataDump.EntityIRI)
+                || node.ToSafeString().StartsWith(Properties.WikidataDump.PropertyIRI);
+        }
+
+        public static bool IsEntity(this INode node)
+        {
+            if (node.GetType() != typeof(UriNode)) return false;
+            return ((UriNode)node).Uri.ToSafeString().Contains(Properties.WikidataDump.EntityIRI);
+        }
+
+        public static bool IsProperty(this INode node)
+        {
+            if (node.GetType() != typeof(UriNode)) return false;
+            return ((UriNode)node).Uri.ToSafeString().Contains(Properties.WikidataDump.PropertyIRI);
+        }
+
+        private static string GetQ(this INode node)
+        {
+            return ((UriNode)node).Uri.Segments.Last();
+        }
+
+        private static string GetQCode(this INode node)
+        {
+            return node.GetQ().Replace(Properties.WikidataDump.EntityPrefix, string.Empty);
+        }
+
+        public static int EntityQCode(this INode node)
+        {
+            var index = node.GetQCode();
+            var parsed = int.TryParse(index, out int indexInt);
+            return parsed ? indexInt : 0;
+        }
+    }
+}
---
+++ SparqlForHumans.UnitTests/RDFExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using SparqlForHumans.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using Xunit;
+
+namespace SparqlForHumans.UnitTests
+{
+    public class RDFExtensionsTests
+    {
+        [Fact]
+        public void TestGetTriple()
+        {
+            var line = "<http://www.wikidata.org/entity/Q27> <http://www.w3.org/2000/01/rdf-schema#label> \"Ireland\"@en .";
+            var triple = line.GetTriple();
+
+            Assert.NotNull(triple);
+            Assert.IsType<Triple>(triple);
+        }
+
+        [Fact]
+        public void TestFailGetTriple()
+        {
+            var line = "hola";
+            Assert.Throws<RdfParseException>(() => line.GetTriple());
+        }
+
+        [Fact]
+        public void TestIsUriNode()
+        {
+            var line = "<http://www.wikidata.org/entity/Q27> <http://www.w3.org/2000/01/rdf-schema#label> \"Ireland\"@en .";
+            var triple = line.GetTriple();
+
+            Assert.True(triple.Subject.IsUriNode());
+            Assert.True(triple.Predicate.IsUriNode());
+            Assert.False(triple.Object.IsUriNode());
+        }
+
+        [Fact]
+        public void TestIsEntity()
+        {
+            var line = "<http://www.wikidata.org/entity/Q27> <http://www.w3.org/2000/01/rdf-schema#label> \"Ireland\"@en .";
+            var triple = line.GetTriple();
+
+            Assert.True(triple.Subject.IsEntity());
+            Assert.False(triple.Predicate.IsEntity());
+            Assert.False(triple.Object.IsEntity());
+
+            line = "<http://www.wikidata.org/entity/Q27> <http://www.wikidata.org/prop/direct/P47> <http://www.wikidata.org/entity/Q26> .";
+            triple = line.GetTriple();
+
+            Assert.True(triple.Subject.IsEntity());
+            Assert.False(triple.Predicate.IsEntity());
+            Assert.True(triple.Object.IsEntity());
+        }
+
+        [Fact]
+        public void TestIsProperty()
+        {
+            var line = "<http://www.wikidata.org/entity/Q27> <http://www.w3.org/2000/01/rdf-schema#label> \"Ireland\"@en .";
+            var triple = line.GetTriple();
+
+            Assert.False(triple.Subject.IsProperty());
+            Assert.False(triple.Predicate.IsProperty());
+            Assert.False(triple.Object.IsProperty());
+
+            line = "<http://www.wikidata.org/entity/Q27> <http://www.wikidata.org/prop/direct/P47> <http://www.wikidata.org/entity/Q26> .";
+            triple = line.GetTriple();
+
+            Assert.False(triple.Subject.IsProperty());
+            Assert.True(triple.Predicate.IsProperty());
+            Assert.False(triple.Object.IsProperty());
+        }
+
+        [Fact]
+        public void TestIsValidSubjectProperty()
+        {
+            var line = "<http://www.wikidata.org/entity/Q27> <http://www.w3.org/2000/01/rdf-schema#label> \"Ireland\"@en .";
+            var triple = line.GetTriple();
+            var isValid = triple.Subject.IsValidSubject();
+            Assert.True(isValid);
+        }
+    }
+}
