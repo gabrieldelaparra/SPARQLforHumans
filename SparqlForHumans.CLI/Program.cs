5c09e497b14239083533c6cc91fa67a0eea3c59d
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-05-30 08:46:40 -0400

    Test to check existing triples on the input dump

--- SparqlForHumans.CLI/Program.cs
+++ SparqlForHumans.CLI/Program.cs
@@ -23,7 +23,8 @@ namespace SparqlForHumans.CLI
     {
         static void Main(string[] args)
         {
-            //var inputFilename = @"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt";
+            var inputFilename = @"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt";
+            FileHelper.ReadLinesTrace(inputFilename, 100000000).ToList();
             //var lines = FileHelper.GetInputLines(inputFilename);
             //int read = 0;
             //foreach (var line in lines)
