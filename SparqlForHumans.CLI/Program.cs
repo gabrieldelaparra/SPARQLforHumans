6c81aa3d22b75a8f2b54c99bd3b50d7002a991b9
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-06-03 21:03:37 -0400

    Filter out 500 first Q instances for real testing

--- SparqlForHumans.CLI/Program.cs
+++ SparqlForHumans.CLI/Program.cs
@@ -1,21 +1,4 @@
-﻿using Lucene.Net.Analysis;
-using Lucene.Net.Analysis.Standard;
-using Lucene.Net.Documents;
-using Lucene.Net.Index;
-using Lucene.Net.QueryParsers;
-using Lucene.Net.Search;
-using Lucene.Net.Store;
-using SparqlForHumans.Core.Services;
-using SparqlForHumans.Core.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using SparqlForHumans.Core.Properties;
-using VDS.RDF;
-using VDS.RDF.Parsing;
-using FileHelper = SparqlForHumans.Core.Utilities.FileHelper;
+﻿using SparqlForHumans.Core.Services;
 
 namespace SparqlForHumans.CLI
 {
@@ -24,21 +7,8 @@ namespace SparqlForHumans.CLI
         static void Main(string[] args)
         {
             var inputFilename = @"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt";
-            //FileHelper.ReadLinesTrace(inputFilename, 100000000).ToList();
-            //var lines = FileHelper.GetInputLines(inputFilename);
-            //int read = 0;
-            //foreach (var line in lines)
-            //{
-            //    read++;
-            //    if(read%100000 == 0)
-            //        Console.WriteLine(read.ToString());
-
-            //    var subject = line.Split(" ").FirstOrDefault();
-            //    if (subject.Contains("/P"))
-            //        Console.WriteLine(subject);
-            //}
-
-            //DumpHelper.FilterTriples()
+            var outputFilename = "filtered-All-500.nt";
+            TriplesFilter.Filter(inputFilename, outputFilename, 500);
 
             //GetLineCount(@"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt.gz");
 
