af1ad250ea33b33b59362601e8833e9734125b85
Gabriel De La Parra <delapa@internal.synopsys.com>
2018-06-12 23:51:46 -0700

    Modify Tests

--- SparqlForHumans.UnitTests/IndexBuilderTests.cs
+++ SparqlForHumans.UnitTests/IndexBuilderTests.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Linq;
+using Lucene.Net.Index;
+using SparqlForHumans.Core.Properties;
 using SparqlForHumans.Core.Services;
 using SparqlForHumans.Core.Utilities;
 using Xunit;
@@ -9,10 +11,9 @@ namespace SparqlForHumans.UnitTests
     public class IndexBuilderTests
     {
         [Fact]
-        public void TestCreateIndex()
+        public void TestCreateBasicIndex()
         {
             var filename = "Resources/filtered.nt";
-            //var filename = "filtered-All-500.nt";
             var outputPath = "Index";
 
             if (Directory.Exists(outputPath))
@@ -24,20 +25,39 @@ namespace SparqlForHumans.UnitTests
 
             Assert.True(Directory.Exists(outputPath));
 
-            var q1 = QueryService.QueryByLabel("Berlin", LuceneHelper.GetLuceneDirectory(outputPath));
-            Assert.NotNull(q1);
-            Assert.Single(q1);
-            Assert.Contains("Berlin", q1.FirstOrDefault().Label);
+            var lines = FileHelper.GetInputLines(filename);
+            var groups = lines.GroupByEntities();
+            var groupsCount = groups.Count();
 
-            var q2 = QueryService.QueryByLabel("Obama", LuceneHelper.GetLuceneDirectory(outputPath));
-            Assert.NotNull(q2);
-            Assert.Empty(q2);
+            using (var reader = IndexReader.Open(LuceneHelper.GetLuceneDirectory(outputPath), true))
+            {
+                var docCount = reader.MaxDoc;
+
+                Assert.Equal(docCount, groupsCount);
+
+                for (var i = 0; i < (docCount > 10 ? 10 : docCount); i++)
+                {
+                    var doc = reader.Document(i);
+                    Assert.NotNull(doc);
+                    Assert.NotEmpty(doc.GetField(Labels.Id.ToString()).StringValue);
+                    Assert.NotEmpty(doc.GetField(Labels.Label.ToString()).StringValue);
+                }
+            }
+
+            //var q1 = QueryService.QueryByLabel("Berlin", LuceneHelper.GetLuceneDirectory(outputPath));
+            //Assert.NotNull(q1);
+            //Assert.Single(q1);
+            //Assert.Contains("Berlin", q1.FirstOrDefault().Label);
+
+            //var q2 = QueryService.QueryByLabel("Obama", LuceneHelper.GetLuceneDirectory(outputPath));
+            //Assert.NotNull(q2);
+            //Assert.Empty(q2);
         }
 
         [Fact]
         public void TestCreateIndex500()
         {
-            var filename = "filtered-All-500.nt";
+            var filename = "Resources/filtered-All-500.nt";
             var outputPath = "Index500";
 
             if (Directory.Exists(outputPath))
