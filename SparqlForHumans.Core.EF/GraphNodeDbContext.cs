e13bdd539b34f32e9674fba91f3e92dbfc979fcd
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-07-23 18:29:26 -0400

    Remove EF

--- SparqlForHumans.Core.EF/GraphNodeDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace SparqlForHumans.Core.EF
-{
-    public class GraphNodeDbContext : DbContext
-    {
-        public DbSet<GraphNode> GraphNodes { get; set; }
-    }
-}
