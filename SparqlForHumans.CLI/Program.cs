923822d1a05ef6ab6acc84ab864b4d454a882001
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-06 19:57:02 -0400

    Refactor for simpler use.

--- SparqlForHumans.CLI/Program.cs
+++ SparqlForHumans.CLI/Program.cs
@@ -8,15 +8,15 @@ namespace SparqlForHumans.CLI
         static void Main(string[] args)
         {
             //var inputFilename = @"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt";
-            //var inputFilename = @"C:\Users\delapa\Desktop\DCC\SparQLforHumans.Dataset\latest-truthy.nt.gz";
+            //
             //var inputFilename = @"C:\Users\delapa\Desktop\DCC\SparQLforHumans.Dataset\filtered-All.nt.gz";
             //var inputFilename = @"filtered-All-2MM.nt";
             //var inputFilename = @"Out-filtered-All-2MM.nt";
-            //var outputFilename = "filtered-All-2MM.nt";
+            //
             //var outputFilename = "filtered-All.nt";
             //var outputPath = "IndexFull";
 
-            //TriplesFilter.Filter(inputFilename, outputFilename, 2000000);
+            //
 
             var inputFilename = @"filtered-All-2MM.nt";
             var outputPath = "Index2MM";
@@ -38,6 +38,25 @@ namespace SparqlForHumans.CLI
             //var res = QueryService.GetLabelFromIndex("Q5", LuceneHelper.LuceneIndexDirectory);
             //var res2 = QueryService.GetLabelFromIndex("P41", LuceneHelper.LuceneIndexDirectory);
             //var res3 = QueryService.QueryByLabel("Obama", LuceneHelper.LuceneIndexDirectory);
+        }
+
+        static void Filter2MM()
+        {
+            var inputFilename = @"C:\Users\delapa\Desktop\DCC\SparQLforHumans.Dataset\latest-truthy.nt.gz";
+            var outputFilename = "filtered-All-2MM.nt";
+            TriplesFilter.Filter(inputFilename, outputFilename, 2000000);
+        }
+
+        static void FilterAll()
+        {
+            var inputFilename = @"C:\Users\delapa\Desktop\DCC\SparQLforHumans.Dataset\latest-truthy.nt.gz";
+            var outputFilename = "filtered-All.nt";
+            TriplesFilter.Filter(inputFilename, outputFilename, -1);
+        }
+
+        static void CreateIndex()
+        {
+            
         }
     }
 }
