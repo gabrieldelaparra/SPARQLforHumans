793078523854bbe4b30ed1425baaeb085538c0c7
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-06 19:59:40 -0400

    Refactor and clean.

--- SparqlForHumans.CLI/Program.cs
+++ SparqlForHumans.CLI/Program.cs
@@ -7,37 +7,9 @@ namespace SparqlForHumans.CLI
     {
         static void Main(string[] args)
         {
-            //var inputFilename = @"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt";
-            //
-            //var inputFilename = @"C:\Users\delapa\Desktop\DCC\SparQLforHumans.Dataset\filtered-All.nt.gz";
-            //var inputFilename = @"filtered-All-2MM.nt";
-            //var inputFilename = @"Out-filtered-All-2MM.nt";
-            //
-            //var outputFilename = "filtered-All.nt";
-            //var outputPath = "IndexFull";
-
-            //
-
-            var inputFilename = @"filtered-All-2MM.nt";
-            var outputPath = "Index2MM";
-
-            if (Directory.Exists(outputPath))
-                Directory.Delete(outputPath, true);
-
-            IndexBuilder.CreateEntitiesIndex(inputFilename, outputPath);
-
-            //GetLineCount(@"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt.gz");
-
-            //FilterTriples(@"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt.gz", @"C:\Users\admin\Desktop\DCC\SparqlForHumans\Out\filtered-triples.nt");
-            //FilterTriples(@"C:\Users\admin\Desktop\DCC\latest-truthy.nt-gz\latest-truthy.nt", @"C:\Users\admin\Desktop\DCC\SparqlForHumans\Out\filtered-triples.nt");
-
-            //CreateLuceneIndex(@"C:\Users\admin\Desktop\DCC\SparqlForHumans\Out\filtered-triples.nt");
-
-            //Optimize();
-
-            //var res = QueryService.GetLabelFromIndex("Q5", LuceneHelper.LuceneIndexDirectory);
-            //var res2 = QueryService.GetLabelFromIndex("P41", LuceneHelper.LuceneIndexDirectory);
-            //var res3 = QueryService.QueryByLabel("Obama", LuceneHelper.LuceneIndexDirectory);
+            //Filter2MM();
+            //FilterAll();
+            //CreateIndex2MM();
         }
 
         static void Filter2MM()
