aa7cc246e0df140bc7d61d6ac2cb25609d98972f
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-06 21:32:16 -0400

    Get Properties Frequency

---
+++ SparqlForHumans.Core/Services/PropertiesFrequency.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SparqlForHumans.Core.Properties;
+using SparqlForHumans.Core.Utilities;
+
+namespace SparqlForHumans.Core.Services
+{
+    public static class PropertiesFrequency
+    {
+        private static readonly NLog.Logger Logger = Utilities.Logger.Init();
+        public static int NotifyTicks { get; } = 1000;
+
+        public static Dictionary<string, int> GetPropertiesFrequency(string triplesFilename)
+        {
+            var lines = FileHelper.GetInputLines(triplesFilename);
+            var groups = lines.GroupBySubject();
+
+            var nodeCount = 0;
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var group in groups)
+            {
+                if (nodeCount % NotifyTicks == 0)
+                    Logger.Info($"Group: {nodeCount:N0}");
+
+                foreach (var line in group)
+                {
+                    var predicateId = line.GetTriple().Predicate.GetId();
+
+                    if (!predicateId.Contains(WikidataDump.PropertyPrefix)) continue;
+
+                    dictionary[predicateId]++;
+                }
+                nodeCount++;
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ SparqlForHumans.UnitTests/PropertyFrequencyTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SparqlForHumans.Core.Services;
+using Xunit;
+
+namespace SparqlForHumans.UnitTests
+{
+    public class PropertyFrequencyTests
+    {
+        [Fact]
+        public void TestGetFileFrequency()
+        {
+            var filename = @"Resources/PropertyFrequencies.nt";
+            Assert.True(File.Exists(filename));
+
+            var dictionary = PropertiesFrequency.GetPropertiesFrequency(filename);
+            
+            Assert.NotNull(dictionary);
+
+            //Assert.Equal(3, dictionary.Count);
+
+            //Assert.Equal("P17", dictionary.ElementAt(0).Key);
+            //Assert.Equal(4, dictionary.ElementAt(0).Value);
+
+        }
+    }
+}
