c97f9f29160009cd7281952b6587e924fb09535c
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-12 22:12:58 -0300

    Document.Get returns null instead of string.empty. Arguments for Enum added.

---
+++ SparqlForHumans.Core/Utilities/DocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Lucene.Net.Documents;
+using SparqlForHumans.Core.Properties;
+
+namespace SparqlForHumans.Core.Utilities
+{
+    public static class DocumentExtensions
+    {
+        private static string GetValue(this Document document, string labelValue)
+        {
+            var documentValue = document.Get(labelValue);
+            return documentValue ?? string.Empty;
+        }
+
+        public static string GetValue(this Document document, Labels label)
+        {
+            return document.GetValue(label.ToString());
+        }
+
+        public static string[] GetValues(this Document document, Labels label)
+        {
+            return document.GetValues(label.ToString());
+        }
+    }
+}
