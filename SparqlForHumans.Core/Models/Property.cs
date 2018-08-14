5b02804762043bb12ad39bc07c91605eefe4b97b
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-14 16:50:38 -0300

    impleemnt ToString()

--- SparqlForHumans.Core/Models/Property.cs
+++ SparqlForHumans.Core/Models/Property.cs
@@ -5,5 +5,10 @@
         public string Id { get; set; }
         public string Label { get; set; }
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Label} -> {Value}";
+        }
     }
 }
