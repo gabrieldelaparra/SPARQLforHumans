63c94359a2614a52e829e9a988095107273a293f
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-08-26 14:27:16 -0300

    Refactor DeleteIfExists to single line as extension method

---
+++ SparqlForHumans.Core/Utilities/PathExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SparqlForHumans.Core.Utilities
+{
+    public static class PathExtensions
+    {
+        public static void DeleteIfExists(this string path)
+        {
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+        }
+    }
+}
