c6a0acbdb38095d9ac340d3f3ab651ec74263e0b
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-09-30 15:00:49 -0300

    Add Base components

---
+++ SparqlForHumans.Web.Client/Components/PropertyComponentBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparqlForHumans.Web.Client.Components
+{
+    public class PropertyComponentBase
+    {
+    }
+}
