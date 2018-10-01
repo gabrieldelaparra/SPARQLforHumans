1c19a86df95e3d25fe30fb71289daf89214756b4
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-01 19:50:42 -0300

    Add base components. Key inputs working.

---
+++ SparqlForHumans.Web/Components/EntityComponentBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Components;
+using SparqlForHumans.Models;
+
+namespace SparqlForHumans.Web.Components
+{
+    public class EntityComponentBase
+    {
+        [Parameter]
+        public string Header { get; set; } = string.Empty;
+
+        public Property[] properties = new Property[0];
+
+        int i = 0;
+        public void addProperty()
+        {
+            properties = properties.Append(new Property()
+            {
+                Id = i++.ToString(),
+            }).ToArray();
+        }
+
+
+        public void removeProperty()
+        {
+            properties = properties.ToList().Take(properties.Length - 1).ToArray();
+        }
+    }
+}
