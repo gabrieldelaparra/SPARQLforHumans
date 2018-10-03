f5831f32cdbeba41ea0cd7b212f617a3d12ccac6
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-03 17:35:14 -0300

    Loads autocomplete on component render. Cool

--- SparqlForHumans.Blazor.App/Services/QueryServices.cs
+++ SparqlForHumans.Blazor.App/Services/QueryServices.cs
@@ -10,14 +10,6 @@ namespace SparqlForHumans.Blazor.App.Services
     public static class QueryServices
     {
         [JSInvokable]
-        public static Task<string[]> RunQuery(string query)
-        {
-            // Call a web api to get some data
-            return Task.FromResult(MultiDocumentQueries.QueryEntitiesByLabel(query)
-                .Select(x => x.Label).ToArray());
-        }
-
-        [JSInvokable]
         public static Task<int[]> ReturnArrayAsync()
         {
             return Task.FromResult(new int[] { 1, 2, 3 });
