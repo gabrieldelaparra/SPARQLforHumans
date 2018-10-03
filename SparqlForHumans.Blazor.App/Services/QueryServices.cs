e2137032b0825daccf7e931b6a6f45e36219a302
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-02 21:45:33 -0300

    still not able to make it run the autocomplete

---
+++ SparqlForHumans.Blazor.App/Services/QueryServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using SparqlForHumans.Core.Services;
+
+namespace SparqlForHumans.Blazor.App.Services
+{
+    public static class QueryServices
+    {
+        [JSInvokable]
+        public static Task<string[]> RunQuery(string query)
+        {
+            return Task.FromResult(MultiDocumentQueries.QueryEntitiesByLabel(query).Select(x=>x.Label).ToArray());
+        }
+    }
+}
