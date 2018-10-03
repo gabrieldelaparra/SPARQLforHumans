fb1701f8fdd4c4f01472336c7197cc4015f4285d
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-03 18:03:24 -0300

    Passing label and value to autocomplete

---
+++ SparqlForHumans.Blazor.App/Shared/SelectableValue.cs
@@ -0,0 +1,8 @@
+﻿namespace SparqlForHumans.Blazor.App.Shared
+{
+    public class SelectableValue
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+}
