cb07d0a7cca9afac9d6ef1b172961c334ecd01f5
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-07 12:53:20 -0300

    Added selectable models

---
+++ SparqlForHumans.Blazor.App/Models/SelectableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SparqlForHumans.Models;
+
+namespace SparqlForHumans.Blazor.App.Models
+{
+    public class SelectableEntity : Entity, ISelectable
+    {
+        public bool IsSelected { get; set; }
+
+        public new List<SelectableProperty> Properties { get; set; } = new List<SelectableProperty>();
+    }
+}
---
+++ SparqlForHumans.Blazor.App/Models/SelectableProperty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SparqlForHumans.Models;
+
+namespace SparqlForHumans.Blazor.App.Models
+{
+    public class SelectableProperty : Property, ISelectable
+    {
+        public bool IsSelected { get; set; }
+    }
+}
