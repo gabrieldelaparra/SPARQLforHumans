185179f2b58ecf5403e0248385b2208eeecc923c
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-07 20:55:46 -0300

    Working so fat

--- SparqlForHumans.Blazor.App/Components/AutoCompleteBase.cs
+++ SparqlForHumans.Blazor.App/Components/AutoCompleteBase.cs
@@ -1,46 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Components;
-using Microsoft.JSInterop;
 using SparqlForHumans.Blazor.App.Shared;
-using SparqlForHumans.Core.Services;
 
 namespace SparqlForHumans.Blazor.App.Components
 {
     public class AutoCompleteBase : BlazorComponent
     {
-        private System.Threading.Timer _timer;
+        private string _inputQuery;
+        private SelectableValue[] _queryResult;
 
-        /// <summary>
-        /// Debounce reset timer and after last item recieved give you last item.
-        /// <exception cref="http://demo.nimius.net/debounce_throttle/">See this example for understanding what is RateLimiting and Debounce</exception>
-        /// </summary>
-        /// <param name="obj">Your object</param>
-        /// <param name="interval">Milisecond interval</param>
-        /// <param name="debounceAction">Called when last item call this method and after interval was finished</param>
-        public void Debounce(int interval, Task action, string param = null)
+        public SelectableValue[] QueryResults
         {
-            SelectableValue[] toReturn = null;
-            _timer?.Dispose();
-            _timer = new System.Threading.Timer(state =>
+            get { return _queryResult; }
+            set
             {
-                _timer.Dispose();
-                if (_timer != null)
-                {
-                    action?.Start();
-                }
-
-                _timer = null;
-            }, param, interval, interval);
+                _queryResult = value;
+                StateHasChanged();
+            }
         }
 
-        private string _inputQuery;
-        private SelectableValue[] queryResults = new SelectableValue[0];
 
         protected string InputQuery
         {
@@ -60,29 +39,19 @@ namespace SparqlForHumans.Blazor.App.Components
 
         private async Task ValidateText(Func<string, Task<SelectableValue[]>> function)
         {
-
             if (isBusyProcessing)
                 return;
 
             isBusyProcessing = true;
-            await Task.Delay(2000);
+            await Task.Delay(Delay);
             isBusyProcessing = false;
 
             Console.WriteLine("Invoke");
-            queryResults = await function?.Invoke(InputQuery);
+            QueryResults = await function?.Invoke(InputQuery);
 
         }
 
-        
-
-        protected ElementRef InputElementRef { get; set; }
-        protected DotNetObjectRef DotNetObjectRef { get; set; }
-
         private string _placeHolder;
-        private Func<string, Task<SelectableValue[]>> _autocompleteSourceProvider;
-        private int _minimumLength;
-        private int _delay;
-        private Action<object> _onSelectionChanged;
 
         [Parameter]
         protected string PlaceHolder
@@ -96,79 +65,16 @@ namespace SparqlForHumans.Blazor.App.Components
         }
 
         [Parameter]
-        protected int Delay
-        {
-            get => _delay;
-            set
-            {
-                _delay = value;
-                //JSRuntime.Current.InvokeAsync<object>("autoCompleteElement.setDelay", InputElementRef, _delay);
-            }
-        }
+        protected int Delay { get; set; }
 
         [Parameter]
-        protected int MinimumLength
-        {
-            get => _minimumLength;
-            set
-            {
-                _minimumLength = value;
-                //JSRuntime.Current.InvokeAsync<object>("autoCompleteElement.setMinLength", InputElementRef, _minimumLength);
-            }
-        }
+        protected int MinimumLength { get; set; }
 
-        protected override void OnAfterRender()
-        {
-            DotNetObjectRef = new DotNetObjectRef(this);
-
-            //JSRuntime.Current.InvokeAsync<object>("autoCompleteElement.initAutoComplete", InputElementRef);
-
-            //MinimumLength = _minimumLength;
-            //Delay = _delay;
-            //AutocompleteSourceProvider = _autocompleteSourceProvider;
-            //OnSelectionChanged = _onSelectionChanged;
-
-            base.OnAfterRender();
-        }
-
-        //[JSInvokable]
-        //public void OnSelectEventListener(object selectedJSONObject)
-        //{
-        //    OnSelectionChanged?.Invoke(selectedJSONObject);
-        //}
-
-        [JSInvokable]
-        public void AutocompleteSourceDelegate(string query)
-        {
-            Console.WriteLine("METHOD INVOKED: " + query);
-        }
-
-        //[JSInvokable]
-        //public Task<SelectableValue[]> AutocompleteSourceDelegate(string query)
-        //{
-        //    return AutocompleteSourceProvider?.Invoke(query);
-        //}
 
         [Parameter]
-        protected Action<object> OnSelectionChanged
-        {
-            get => _onSelectionChanged;
-            set
-            {
-                _onSelectionChanged = value;
-                //JSRuntime.Current.InvokeAsync<object>("autoCompleteElement.setSelect", InputElementRef, DotNetObjectRef);
-            }
-        }
+        protected Action<object> OnSelectionChanged { get; set; }
 
         [Parameter]
         protected Func<string, Task<SelectableValue[]>> AutocompleteSourceProvider { get; set; }
-        //{
-        //    get => _autocompleteSourceProvider;
-        //    set
-        //    {
-        //        _autocompleteSourceProvider = value;
-        //        //JSRuntime.Current.InvokeAsync<object>("autoCompleteElement.setSourceFunction", InputElementRef, DotNetObjectRef);
-        //    }
-        //}
     }
 }
