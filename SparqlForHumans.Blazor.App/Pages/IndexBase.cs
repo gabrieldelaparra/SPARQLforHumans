d419d9bb404d0acc1541a44ab232b4d2d55e9f77
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-09 11:56:06 -0300

    Add basic add button

---
+++ SparqlForHumans.Blazor.App/Pages/IndexBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace SparqlForHumans.Blazor.App.Pages
+{
+    public class IndexBase : BlazorComponent
+    {
+    }
+}
