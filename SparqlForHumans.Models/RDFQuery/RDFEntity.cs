34cc93b2dd71710118ac17070e32c0ec98a6bb79
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-09 23:26:19 -0300

    added constructor based on id and label, for rdf querying. related to #11

--- SparqlForHumans.Models/RDFQuery/RDFEntity.cs
+++ SparqlForHumans.Models/RDFQuery/RDFEntity.cs
@@ -4,6 +4,13 @@ namespace SparqlForHumans.Models.RDFQuery
 {
     public class RDFEntity : Entity, IQueriableSubject
     {
+        public RDFEntity(){}
+
+        public RDFEntity(ISubject iSubject)
+        {
+            Id = iSubject.Id;
+        }
+
         public string Uri()
         {
             return this.WikidataUri();
--- SparqlForHumans.Models/RDFQuery/RDFProperty.cs
+++ SparqlForHumans.Models/RDFQuery/RDFProperty.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using SparqlForHumans.Models.Extensions;
+﻿using SparqlForHumans.Models.Extensions;
 
 namespace SparqlForHumans.Models.RDFQuery
 {
     public class RDFProperty : Property, IQueriableSubject
     {
+        public RDFProperty() {}
+
+        public RDFProperty(ISubject iSubject)
+        {
+            Id = iSubject.Id;
+        }
+
         public string Uri()
         {
             return this.WikidataUri();
