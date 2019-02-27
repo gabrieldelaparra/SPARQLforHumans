b42efacfa6eadfa5bd3dd6c904ced55be9ae9f81
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-02-27 20:59:41 -0300

    for the dictionary extensions. All fail, require implementation.

---
+++ SparqlForHumans.UnitTests/DictionaryExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SparqlForHumans.UnitTests
+{
+    public static class DictionaryExtensionsTests
+    {
+        [Fact]
+        public static void TestAddSafeSameType()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public static void TestAddSafeDifferentTypes()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public static void TestInvertDictionarySameType()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public static void TestInvertDictionaryDifferentTypes()
+        {
+            Assert.False(true);
+        }
+    }
+}
