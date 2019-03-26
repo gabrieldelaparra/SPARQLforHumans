fc435754798d445d447a6912f582a239c187179f
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-03-26 15:53:18 -0300

    add path extensions tests. Fix #76

--- SparqlForHumans.UnitTests/Utilities/PathExtensionsTests.cs
+++ SparqlForHumans.UnitTests/Utilities/PathExtensionsTests.cs
@@ -1,13 +1,47 @@
-﻿using Xunit;
+﻿using System.IO;
+using SparqlForHumans.Utilities;
+using Xunit;
 
 namespace SparqlForHumans.UnitTests.Utilities
 {
     public class PathExtensionsTests
     {
         [Fact]
-        public void TestDeleteIfExists()
+        public void TestDeleteIfExistsDirectory()
         {
-            Assert.False(true);
+            var path = "tempFolder";
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+            
+            Directory.CreateDirectory(path);
+            Assert.True(Directory.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+        }
+
+        [Fact]
+        public void TestDeleteIfExistsFile()
+        {
+            var path = "tempFile";
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
+
+            using (var file = File.Create(path))
+            {
+            }
+            Assert.True(File.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
         }
     }
 }
