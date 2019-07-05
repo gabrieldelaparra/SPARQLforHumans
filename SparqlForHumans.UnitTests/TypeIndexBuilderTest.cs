071e6159d23ba2799447f0c7951b94801fe2a4af
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-07-06 00:49:33 +0200

    Remove unused functionality

--- SparqlForHumans.UnitTests/TypeIndexBuilderTest.cs
+++ SparqlForHumans.UnitTests/TypeIndexBuilderTest.cs
@@ -2,30 +2,30 @@
 
 namespace SparqlForHumans.UnitTests
 {
+    /// <summary>
+    ///     Given the following nTriples file:
+    ///     Q76 (Obama) -> P31 (InstanceOf) -> Q5 (Human)
+    ///     Q76 (Obama) -> P27 -> Qxx
+    ///     Q76 (Obama) -> P555 -> Qxx
+    ///     ...
+    ///     Q77 (Other Human) -> P31 (InstanceOf) -> Q5 (Human)
+    ///     Q77 (Other Human) -> P33 -> Qxx
+    ///     Q77 (Other Human) -> P44 -> Qxx
+    ///     ...
+    ///     Q5 (Human)
+    ///     ...
+    ///     Q278 (Chile) -> P31 (InstanceOf) -> Q17 (Country)
+    ///     Q278 (Chile) -> P555 -> Qxx
+    ///     Q278 (Chile) -> P777 -> Qxx
+    ///     ...
+    ///     Q17 (Country)
+    ///     ...
+    ///     Should return a dictionary with the following:
+    ///     (Human) Q5 (Obama + OtherHuman): P31, P27, P555, P33, P44
+    ///     (Country) Q17 (Chile): P31, P555, P777
+    /// </summary>
     public class TypeIndexBuilderTest
     {
-        /// <summary>
-        ///     Given the following nTriples file:
-        ///     Q76 (Obama) -> P31 (InstanceOf) -> Q5 (Human)
-        ///     Q76 (Obama) -> P27 -> Qxx
-        ///     Q76 (Obama) -> P555 -> Qxx
-        ///     ...
-        ///     Q77 (Other Human) -> P31 (InstanceOf) -> Q5 (Human)
-        ///     Q77 (Other Human) -> P33 -> Qxx
-        ///     Q77 (Other Human) -> P44 -> Qxx
-        ///     ...
-        ///     Q5 (Human)
-        ///     ...
-        ///     Q278 (Chile) -> P31 (InstanceOf) -> Q17 (Country)
-        ///     Q278 (Chile) -> P555 -> Qxx
-        ///     Q278 (Chile) -> P777 -> Qxx
-        ///     ...
-        ///     Q17 (Country)
-        ///     ...
-        ///     Should return a dictionary with the following:
-        ///     (Human) Q5 (Obama + OtherHuman): P31, P27, P555, P33, P44
-        ///     (Country) Q17 (Chile): P31, P555, P777
-        /// </summary>
         [Fact]
         public static void TestCreateEntitiesTypesPropertiesDictionary()
         {
