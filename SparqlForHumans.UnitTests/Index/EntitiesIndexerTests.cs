5407fb230245901445859087bad9f3473bf740dc
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-07-05 02:00:05 +0200

    Add tests to entities indexing. Closes #104

--- SparqlForHumans.UnitTests/Index/EntitiesIndexerTests.cs
+++ SparqlForHumans.UnitTests/Index/EntitiesIndexerTests.cs
@@ -20,22 +20,49 @@ namespace SparqlForHumans.UnitTests.Index
 
             Assert.False(Directory.Exists(outputPath));
 
-            var indexer = new EntitiesIndexer(filename, outputPath);
-            indexer.Index();
+            new EntitiesIndexer(filename, outputPath).Index();
 
             using (var luceneDirectory = FSDirectory.Open(outputPath.GetOrCreateDirectory()))
             {
                 Assert.True(Directory.Exists(outputPath));
 
-                var q1 = MultiDocumentQueries.QueryEntitiesByLabel("Berlin", luceneDirectory);
-                Assert.NotNull(q1);
-                Assert.Contains("Berlin", q1.FirstOrDefault().Label);
-
-                var q2 = MultiDocumentQueries.QueryEntitiesByLabel("Obama", luceneDirectory);
-                Assert.NotNull(q2);
-                Assert.Contains("Barack Obama", q2.FirstOrDefault().Label);
+                var queryBerlin = MultiDocumentQueries.QueryEntitiesByLabel("Berli", luceneDirectory).ToArray();
+                Assert.NotEmpty(queryBerlin);
+                var result = queryBerlin[0];
+                Assert.Equal("Q64", result.Id);
+                Assert.Equal("Berlin", result.Label);
+                Assert.Equal("capital city of Germany", result.Description);
+                Assert.Contains("Berlin, Germany", result.AltLabels);
+                var properties = result.Properties.Select(x => x.Id).ToArray();
+                Assert.Contains("P17", properties);
+                Assert.Contains("P1376", properties);
+                Assert.False(result.IsType);
+                Assert.Contains("Q515", result.InstanceOf);
+                Assert.Contains("Q5119", result.InstanceOf);
+                Assert.Contains("Q999",result.SubClass);
             }
 
+            outputPath.DeleteIfExists();
+        }
+
+        [Fact]
+        public void TestIndexHasTypes()
+        {
+            const string filename = "Resources/EntityTypes.nt";
+            const string outputPath = "IndexTypes";
+
+            outputPath.DeleteIfExists();
+
+            Assert.False(Directory.Exists(outputPath));
+
+            new EntitiesIndexer(filename, outputPath).Index();
+
+            using (var luceneDirectory = FSDirectory.Open(outputPath.GetOrCreateDirectory()))
+            {
+                var typesQuery = MultiDocumentQueries.QueryEntitiesByLabel("*", luceneDirectory, true).ToArray();
+                Assert.NotEmpty(typesQuery);
+                Assert.Contains(typesQuery, x =>x.Id.Equals("Q5"));
+            }
             outputPath.DeleteIfExists();
         }
     }
