a674163bdf0fef263e8c88734c0b94b903dc06bb
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-07-14 21:17:48 +0200

    Move file, remove comments

---
+++ SparqlForHumans.Server/Controllers/MultiEntityQueryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SparqlForHumans.Lucene;
+using SparqlForHumans.Lucene.Extensions;
+using SparqlForHumans.Lucene.Queries;
+
+namespace SparqlForHumans.Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/MultiEntityQuery")]
+    public class MultiEntityQueryController : Controller
+    {
+        public IActionResult Run(string term)
+        {
+            var filteredItems = new MultiLabelEntityQuery(LuceneDirectoryDefaults.EntityIndexPath, term).Query();
+            filteredItems.AddProperties();
+
+            return Json(filteredItems);
+        }
+    }
+}
