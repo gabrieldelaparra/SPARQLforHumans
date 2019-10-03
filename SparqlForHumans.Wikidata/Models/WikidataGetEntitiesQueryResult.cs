451db1b133930c6cf2a09a6dba39483b26c1b053
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-10-03 21:40:24 +0200

    Implement Wikidata Query API to do search and get queries

---
+++ SparqlForHumans.Wikidata/Models/WikidataGetEntitiesQueryResult.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SparqlForHumans.Wikidata.Models
+{
+    public class Entity
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("datatype")]
+        public string Datatype { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("labels")]
+        public Property Labels { get; set; }
+
+        [JsonProperty("descriptions")]
+        public Property Descriptions { get; set; }
+    }
+
+    public class Property
+    {
+        [JsonProperty("en")]
+        public LanguageValues En { get; set; }
+    }
+
+    public class LanguageValues
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+    public class WikidataGetEntitiesQueryResult
+    {
+        [JsonProperty("entities")]
+        public Dictionary<string, Entity> Entities { get; set; }
+
+        [JsonProperty("success")]
+        public long Success { get; set; }
+    }
+}
---
+++ SparqlForHumans.Wikidata/Models/WikidataSearchEntitiesResult.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SparqlForHumans.Wikidata.Models
+{
+    public class WikidataSearchEntitiesResult
+    {
+        [JsonProperty("searchinfo")]
+        public Searchinfo Searchinfo { get; set; }
+
+        [JsonProperty("search")]
+        public Search[] Search { get; set; }
+
+        [JsonProperty("search-continue", NullValueHandling = NullValueHandling.Ignore)]
+        public long SearchContinue { get; set; }
+
+        [JsonProperty("success")]
+        public long Success { get; set; }
+    }
+
+    public class Search
+    {
+        [JsonProperty("repository")]
+        public string Repository { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("concepturi")]
+        public Uri Concepturi { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("pageid")]
+        public long Pageid { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("match")]
+        public Match Match { get; set; }
+
+        [JsonProperty("aliases", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Aliases { get; set; }
+    }
+
+    public class Match
+    {
+        [JsonProperty("type")]
+        public TypeEnum Type { get; set; }
+
+        [JsonProperty("language")]
+        public Language Language { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class Searchinfo
+    {
+        [JsonProperty("search")]
+        public string Search { get; set; }
+    }
+
+    public enum Language { En };
+
+    public enum TypeEnum { Alias, Label };
+}
