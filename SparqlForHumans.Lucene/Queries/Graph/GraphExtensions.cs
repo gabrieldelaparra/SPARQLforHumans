0b91851f0d71cb6991141d69a15804b87aeb9c71
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-11-05 23:02:43 +0100

    Refactor Graph Traversal to a new class

--- SparqlForHumans.Lucene/Queries/Graph/GraphExtensions.cs
+++ SparqlForHumans.Lucene/Queries/Graph/GraphExtensions.cs
@@ -8,6 +8,11 @@ namespace SparqlForHumans.Lucene.Queries.Graph
 {
     public static class GraphExtensions
     {
+        internal static bool HasInstanceOf(this QueryEdge edge)
+        {
+            return edge.uris.HasInstanceOf();
+        }
+
         internal static bool HasInstanceOf(this string[] uris)
         {
             return uris.Any(IsInstanceOf);
@@ -20,6 +25,14 @@ namespace SparqlForHumans.Lucene.Queries.Graph
         {
             return node.GetOutgoingEdges(graph)?.Where(x => x.IsInstanceOf);
         }
+        /// <summary>
+        /// For a given node, get the InstanceOf edges, and get their types;
+        /// TODO: I think that this is wrong, this will get the URIs of the edges, not of the connected node.
+        /// TODO: Create some tests to expose this.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="graph"></param>
+        /// <returns></returns>
         internal static IEnumerable<string> GetInstanceOfValues(this QueryNode node, QueryGraph graph)
         {
             return node.GetInstanceOfEdges(graph)?.SelectMany(x => x.uris);
@@ -34,13 +47,15 @@ namespace SparqlForHumans.Lucene.Queries.Graph
         }
         internal static IEnumerable<QueryEdge> GetOutgoingEdges(this QueryNode node, QueryGraph graph)
         {
-            return graph.Edges.Select(x=>x.Value).Where(x => x.sourceId.Equals(node.id));
+            return graph.Edges.Select(x => x.Value).Where(x => x.sourceId.Equals(node.id));
         }
 
         internal static IEnumerable<QueryEdge> GetIncommingEdges(this QueryNode node, QueryGraph graph)
         {
-            return graph.Edges.Select(x=>x.Value).Where(x => x.targetId.Equals(node.id));
+            return graph.Edges.Select(x => x.Value).Where(x => x.targetId.Equals(node.id));
         }
+
+
 
         internal static IEnumerable<QueryNode> GetOutgoingNodes(this QueryNode node, QueryGraph graph)
         {
@@ -49,88 +64,35 @@ namespace SparqlForHumans.Lucene.Queries.Graph
             return edges.Select(x => x.GetTargetNode(graph));
         }
 
-        //TODO: Change name:
-        internal static void ParseQueryType(this QueryNode node, QueryGraph graph)
-        {
-            if (node.uris.Any())
-                node.QueryType = QueryType.ConstantTypeDoNotQuery;
-            else if (node.IsKnownType && node.IsDirectedToKnownType)
-                node.QueryType = QueryType.KnownSubjectAndObjectTypesQueryInstanceEntities;
-            else if (node.IsKnownType)
-                node.QueryType = QueryType.KnownSubjectTypeQueryInstanceEntities;
-            else if (node.IsInferredTypeDomain && node.IsInferredTypeRange)
-                node.QueryType = QueryType.InferredDomainAndRangeTypeEntities;
-            else if (node.IsInferredTypeDomain && node.IsDirectedToKnownType)
-                node.QueryType = QueryType.KnownPredicateAndObject;
-            else if (node.IsInferredTypeDomain)
-                node.QueryType = QueryType.InferredDomainTypeEntities;
-            else if (node.IsInferredTypeRange)
-                node.QueryType = QueryType.InferredRangeTypeEntities;
-            else if (node.IsDirectedToKnownType)
-                node.QueryType = QueryType.KnownObjectTypeNotUsed;
-            else
-                node.QueryType = QueryType.QueryTopEntities;
-        }
-
-        internal static void ParseQueryType(this QueryEdge edge, QueryGraph graph)
-        {
-            var source = edge.GetSourceNode(graph);
-            var target = edge.GetTargetNode(graph);
-
-            if (edge.uris.Any())
-                edge.QueryType = QueryType.ConstantTypeDoNotQuery;
-            else if (source.IsKnownType && target.IsKnownType)
-                edge.QueryType = QueryType.KnownSubjectAndObjectTypesIntersectDomainRangeProperties;
-            else if (source.IsKnownType)
-                edge.QueryType = QueryType.KnownSubjectTypeOnlyQueryDomainProperties;
-            else if (target.IsKnownType)
-                edge.QueryType = QueryType.KnownObjectTypeOnlyQueryRangeProperties;
-            else if (source.IsInferredType && target.IsInferredType)
-            {
-                edge.QueryType = QueryType.InferredDomainAndRangeTypeProperties;
-                edge.Domain = source.InferredTypes;
-                edge.Range = target.InferredTypes;
-            }
-            else if (source.IsInferredType)
-            {
-                edge.QueryType = QueryType.InferredDomainTypeProperties;
-                edge.Domain = source.InferredTypes;
-            }
-            else if (target.IsInferredType)
-            {
-                edge.QueryType = QueryType.InferredRangeTypeProperties;
-                edge.Range = target.InferredTypes;
-            }
-            else
-                edge.QueryType = QueryType.QueryTopProperties;
-        }
-
         internal static void ExploreGraph(this QueryGraph graph, string entitiesIndexPath, string propertyIndexPath)
         {
-            foreach (var edge in graph.Edges.Select(x=>x.Value))
+            //For all properties
+            //If the property, !InstanceOf, get the Domain and Range from the DB;
+            //TODO: Why only !InstanceOf (?)
+            foreach (var edge in graph.Edges.Select(x => x.Value))
             {
-                if (edge.uris.HasInstanceOf())
-                    edge.IsInstanceOf = true;
-                else
-                {
-                    if (string.IsNullOrWhiteSpace(propertyIndexPath)) continue;
-                    var properties = new BatchIdPropertyQuery(propertyIndexPath, edge.uris.Select(x => x.GetUriIdentifier())).Query();
-                    edge.Domain = properties.SelectMany(x => x.Domain).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
-                    edge.Range = properties.SelectMany(x => x.Range).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
-                }
+                if (edge.IsInstanceOf) continue;
+
+                if (string.IsNullOrWhiteSpace(propertyIndexPath)) continue;
+                var properties = new BatchIdPropertyQuery(propertyIndexPath, edge.uris.Select(x => x.GetUriIdentifier())).Query();
+                edge.Domain = properties.SelectMany(x => x.Domain).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
+                edge.Range = properties.SelectMany(x => x.Range).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
             }
 
-            foreach (var node in graph.Nodes.Select(x=>x.Value))
+            //TODO: Need some comments here. Which cases are this. Or some tests.
+            foreach (var node in graph.Nodes.Select(x => x.Value))
             {
                 if (node.GetInstanceOfValues(graph).Any())
                 {
                     node.IsKnownType = true;
                     node.Types = node.GetOutgoingNodes(graph).SelectMany(x => x.uris).Distinct().ToList();
                 }
-                else if (node.uris.Any())
+                else if (node.IsGivenType)
                 {
                     node.IsKnownType = true;
-                    node.Types = new BatchIdEntityQuery(entitiesIndexPath, node.uris.Distinct()).Query().SelectMany(x => x.InstanceOf).ToList();
+                    node.Types = node.uris.ToList();
+                    //if (!string.IsNullOrWhiteSpace(entitiesIndexPath))
+                    //    node.Types = new BatchIdEntityQuery(entitiesIndexPath, node.uris.Distinct()).Query().SelectMany(x => x.InstanceOf).ToList();
                 }
                 else
                 {
@@ -147,49 +109,12 @@ namespace SparqlForHumans.Lucene.Queries.Graph
                 }
             }
 
-            foreach (var node in graph.Nodes.Select(x=>x.Value))
+            //TODO: Need some comments here. Which cases are this. Or some tests.
+            foreach (var node in graph.Nodes.Select(x => x.Value))
             {
                 if (node.GetOutgoingNodes(graph).Any(x => x.IsKnownType))
                     node.IsDirectedToKnownType = true;
             }
-
-        }
-
-        internal static void TraverseDepthFirstNode(this QueryGraph graph, int nodeId)
-        {
-            // Get the Node
-            var node = graph.Nodes.FirstOrDefault(x => x.Key.Equals(nodeId)).Value;
-            if (node.Traversed) return;
-
-            // Check rules for this Node:
-            node.ParseQueryType(graph);
-
-            // Mark as Checked
-            node.Traversed = true;
-
-            // Recursion
-            foreach (var edge in node.GetOutgoingEdges(graph).Where(x => !x.Traversed))
-            {
-                graph.TraverseDepthFirstEdge(edge.id);
-            }
-        }
-
-        internal static void TraverseDepthFirstEdge(this QueryGraph graph, int edgeId)
-        {
-            // Get the Edge
-            var edge = graph.Edges.FirstOrDefault(x => x.Key.Equals(edgeId)).Value;
-            if (edge.Traversed) return;
-
-            // Check rules for this Edge:
-            edge.ParseQueryType(graph);
-
-            // Mark as Checked
-            edge.Traversed = true;
-
-            // Recursion
-            var node = edge.GetTargetNode(graph);
-            if (!node.Traversed)
-                graph.TraverseDepthFirstNode(node.id);
         }
     }
 
---
+++ SparqlForHumans.Lucene/Queries/Graph/GraphTraversal.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparqlForHumans.Lucene.Queries.Graph
+{
+    public static class GraphTraversal
+    {
+        internal static void TraverseGraph(this QueryGraph graph)
+        {
+            //TODO: Need some comments here. Which cases are this. Or some tests.
+            //For all Entities:
+            //If the node has a GivenType, just use that type.
+            //If the node is connected to a InstanceOf Node, get those Types.
+            //
+            foreach (var node in graph.Nodes.Select(x => x.Value))
+            {
+                if (node.GetInstanceOfValues(graph).Any())
+                {
+                    node.IsKnownType = true;
+                    node.Types = node.GetOutgoingNodes(graph).SelectMany(x => x.uris).Distinct().ToList();
+                }
+                else if (node.IsGivenType)
+                {
+                    node.IsKnownType = true;
+                    node.Types = node.uris.ToList();
+                    //if (!string.IsNullOrWhiteSpace(entitiesIndexPath))
+                    //    node.Types = new BatchIdEntityQuery(entitiesIndexPath, node.uris.Distinct()).Query().SelectMany(x => x.InstanceOf).ToList();
+                }
+                else
+                {
+                    if (node.GetOutgoingEdges(graph).Any(x => x.Domain.Any()))
+                    {
+                        node.IsInferredTypeDomain = true;
+                        node.InferredTypes = node.InferredTypes.Union(node.GetOutgoingEdges(graph).SelectMany(x => x.Domain)).ToList();
+                    }
+                    if (node.GetIncommingEdges(graph).Any(x => x.Range.Any()))
+                    {
+                        node.IsInferredTypeRange = true;
+                        node.InferredTypes = node.InferredTypes.Union(node.GetIncommingEdges(graph).SelectMany(x => x.Range)).ToList();
+                    }
+                }
+            }
+
+            //TODO: Need some comments here. Which cases are this. Or some tests.
+            foreach (var node in graph.Nodes.Select(x => x.Value))
+            {
+                if (node.GetOutgoingNodes(graph).Any(x => x.IsKnownType))
+                    node.IsDirectedToKnownType = true;
+            }
+        }
+        internal static void TraverseDepthFirstNode(this QueryGraph graph, int nodeId)
+        {
+            // Get the Node
+            var node = graph.Nodes.FirstOrDefault(x => x.Key.Equals(nodeId)).Value;
+            if (node.Traversed) return;
+
+            // Check rules for this Node:
+            node.ParseQueryType(graph);
+
+            // Mark as Checked
+            node.Traversed = true;
+
+            // Recursion
+            foreach (var edge in node.GetOutgoingEdges(graph).Where(x => !x.Traversed))
+            {
+                graph.TraverseDepthFirstEdge(edge.id);
+            }
+        }
+
+        internal static void TraverseDepthFirstEdge(this QueryGraph graph, int edgeId)
+        {
+            // Get the Edge
+            var edge = graph.Edges.FirstOrDefault(x => x.Key.Equals(edgeId)).Value;
+            if (edge.Traversed) return;
+
+            // Check rules for this Edge:
+            edge.ParseQueryType(graph);
+
+            // Mark as Checked
+            edge.Traversed = true;
+
+            // Recursion
+            var node = edge.GetTargetNode(graph);
+            if (!node.Traversed)
+                graph.TraverseDepthFirstNode(node.id);
+        }
+
+        //TODO: Change name (?)
+        internal static void ParseQueryType(this QueryNode node, QueryGraph graph)
+        {
+            //Given Type:
+            if (node.uris.Any())
+                node.QueryType = QueryType.ConstantTypeDoNotQuery;
+            
+            //Known Types, based on the outgoing P31 edges:
+            else if (node.IsKnownType && node.IsDirectedToKnownType)
+                node.QueryType = QueryType.KnownSubjectAndObjectTypesQueryInstanceEntities;
+            else if (node.IsKnownType)
+                node.QueryType = QueryType.KnownSubjectTypeQueryInstanceEntities;
+            
+            //Inferred Types, based on destination P31:
+            else if (node.IsInferredTypeDomain && node.IsInferredTypeRange)
+                node.QueryType = QueryType.InferredDomainAndRangeTypeEntities;
+            else if (node.IsInferredTypeDomain && node.IsDirectedToKnownType)
+                node.QueryType = QueryType.KnownPredicateAndObjectNotUsed;
+            else if (node.IsInferredTypeDomain)
+                node.QueryType = QueryType.InferredDomainTypeEntities;
+            else if (node.IsInferredTypeRange)
+                node.QueryType = QueryType.InferredRangeTypeEntities;
+            
+            // TODO: Document what is this
+            else if (node.IsDirectedToKnownType)
+                node.QueryType = QueryType.KnownObjectTypeNotUsed;
+
+            //No known case. No given, known or inferred types;
+            else
+                node.QueryType = QueryType.QueryTopEntities;
+        }
+
+        internal static void ParseQueryType(this QueryEdge edge, QueryGraph graph)
+        {
+            var source = edge.GetSourceNode(graph);
+            var target = edge.GetTargetNode(graph);
+
+            //Given type for the edge. Like P31 or others:
+            if (edge.uris.Any())
+                edge.QueryType = QueryType.ConstantTypeDoNotQuery;
+
+            // When the parent node is known, then only the properties available for that node should be displayed (?)
+            // TODO: Add cases for All 3 cases: Source&Destination; Source; Destination
+            else if (source.IsGivenType && target.IsGivenType)
+                edge.QueryType = QueryType.GivenSubjectUrisQueryGivenProperties;
+
+            // TODO: I need some visual aid to describe these cases:
+            else if (source.IsKnownType && target.IsKnownType)
+                edge.QueryType = QueryType.KnownSubjectAndObjectTypesIntersectDomainRangeProperties;
+            else if (source.IsKnownType)
+                edge.QueryType = QueryType.KnownSubjectTypeQueryDomainProperties;
+            else if (target.IsKnownType)
+                edge.QueryType = QueryType.KnownObjectTypeQueryRangeProperties;
+
+            // Inferred Types:
+            else if (source.IsInferredType && target.IsInferredType)
+            {
+                edge.QueryType = QueryType.InferredDomainAndRangeTypeProperties;
+                edge.Domain = source.InferredTypes;
+                edge.Range = target.InferredTypes;
+            }
+            else if (source.IsInferredType)
+            {
+                edge.QueryType = QueryType.InferredDomainTypeProperties;
+                edge.Domain = source.InferredTypes;
+            }
+            else if (target.IsInferredType)
+            {
+                edge.QueryType = QueryType.InferredRangeTypeProperties;
+                edge.Range = target.InferredTypes;
+            }
+            
+            // Nothing found from our search cases:
+            else
+                edge.QueryType = QueryType.QueryTopProperties;
+        }
+    }
+}
