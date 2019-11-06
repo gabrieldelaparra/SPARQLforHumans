bca0275312801be30ca73f9162af4f3adb0462a2
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-11-06 19:43:17 +0100

    Add tests for graph extensions;

--- SparqlForHumans.Lucene/Queries/Graph/GraphExtensions.cs
+++ SparqlForHumans.Lucene/Queries/Graph/GraphExtensions.cs
@@ -8,103 +8,124 @@ namespace SparqlForHumans.Lucene.Queries.Graph
 {
     public static class GraphExtensions
     {
-        internal static bool HasInstanceOf(this QueryEdge edge)
+        /// <summary>
+        /// Given an Edge, checks if the Edge.uris has any value ending with "P31" (InstanceOf)
+        /// </summary>
+        public static bool HasInstanceOf(this QueryEdge edge)
         {
             return edge.uris.HasInstanceOf();
         }
-
-        internal static bool HasInstanceOf(this string[] uris)
+        private static bool HasInstanceOf(this string[] uris)
         {
             return uris.Any(IsInstanceOf);
         }
-        internal static bool IsInstanceOf(this string uri)
+        private static bool IsInstanceOf(this string uri)
         {
             return uri.EndsWith("P31");
         }
-        internal static IEnumerable<Edge> GetInstanceOfEdges(this QueryNode node, QueryGraph graph)
+
+        /// <summary>
+        /// Given a Node, get all the outgoing edges (GetOutgoingEdges) that are InstanceOf.
+        /// </summary>
+        public static IEnumerable<QueryEdge> GetInstanceOfEdges(this QueryNode node, QueryGraph graph)
         {
             return node.GetOutgoingEdges(graph)?.Where(x => x.IsInstanceOf);
         }
+
         /// <summary>
-        /// For a given node, get the InstanceOf edges, and get their types;
-        /// TODO: I think that this is wrong, this will get the URIs of the edges, not of the connected node.
-        /// TODO: Create some tests to expose this.
+        /// Given an Edge, get the Graph's Node, where Node.Id equals Edge.SourceId
         /// </summary>
-        /// <param name="node"></param>
-        /// <param name="graph"></param>
-        /// <returns></returns>
-        internal static IEnumerable<string> GetInstanceOfValues(this QueryNode node, QueryGraph graph)
-        {
-            return node.GetInstanceOfEdges(graph)?.SelectMany(x => x.uris);
-        }
         public static QueryNode GetSourceNode(this QueryEdge edge, QueryGraph graph)
         {
             return graph.Nodes.FirstOrDefault(x => x.Key.Equals(edge.sourceId)).Value;
         }
+
+        /// <summary>
+        /// Given an Edge, get the Graph's Node, where Node.Id equals Edge.TargetId
+        /// </summary>
         public static QueryNode GetTargetNode(this QueryEdge edge, QueryGraph graph)
         {
             return graph.Nodes.FirstOrDefault(x => x.Key.Equals(edge.targetId)).Value;
         }
-        internal static IEnumerable<QueryEdge> GetOutgoingEdges(this QueryNode node, QueryGraph graph)
+
+        /// <summary>
+        /// Given a Node: Get the InstanceOfEdges. Get the TargetNodes. Get their Types;
+        /// </summary>
+        public static IEnumerable<string> GetInstanceOfValues(this QueryNode node, QueryGraph graph)
+        {
+            return node.GetInstanceOfEdges(graph)?.Select(x => x.GetTargetNode(graph)).SelectMany(x => x.Types);
+        }
+
+        /// <summary>
+        /// Get all edges of the graph, where sourceId is equals to the node.id.
+        /// </summary>
+        public static IEnumerable<QueryEdge> GetOutgoingEdges(this QueryNode node, QueryGraph graph)
         {
             return graph.Edges.Select(x => x.Value).Where(x => x.sourceId.Equals(node.id));
         }
 
-        internal static IEnumerable<QueryEdge> GetIncommingEdges(this QueryNode node, QueryGraph graph)
+        /// <summary>
+        /// Get all edges of the graph, where targetId is equals to the node.id.
+        /// </summary>
+        public static IEnumerable<QueryEdge> GetIncomingEdges(this QueryNode node, QueryGraph graph)
         {
             return graph.Edges.Select(x => x.Value).Where(x => x.targetId.Equals(node.id));
         }
 
-
-
-        internal static IEnumerable<QueryNode> GetOutgoingNodes(this QueryNode node, QueryGraph graph)
+        /// <summary>
+        /// Given a node, get all nodes of the graph, where their edge.sourceId is equals to the node.id.
+        /// </summary>
+        public static IEnumerable<QueryNode> GetOutgoingNodes(this QueryNode node, QueryGraph graph)
         {
-            var edges = node.GetOutgoingEdges(graph);
-            if (edges == null) return null;
-            return edges.Select(x => x.GetTargetNode(graph));
+            return node.GetOutgoingEdges(graph)?.Select(x => x.GetTargetNode(graph));
         }
 
+        /// <summary>
+        /// Get all edges of the graph, where their edge.targetId is equals to the node.id.
+        /// </summary>
+        public static IEnumerable<QueryNode> GetIncomingNodes(this QueryNode node, QueryGraph graph)
+        {
+            return node.GetIncomingEdges(graph)?.Select(x => x.GetSourceNode(graph));
+        }
+
+        //TODO: Move out of here.
         internal static void ExploreGraph(this QueryGraph graph, string entitiesIndexPath, string propertyIndexPath)
         {
             //For all properties
+            //TODO: (?) Should it be only for properties with a given type?
             //If the property, !InstanceOf, get the Domain and Range from the DB;
             //TODO: Why only !InstanceOf (?)
             foreach (var edge in graph.Edges.Select(x => x.Value))
             {
-                if (edge.IsInstanceOf) continue;
+                if (!edge.uris.Any()) continue;
 
-                if (string.IsNullOrWhiteSpace(propertyIndexPath)) continue;
                 var properties = new BatchIdPropertyQuery(propertyIndexPath, edge.uris.Select(x => x.GetUriIdentifier())).Query();
                 edge.Domain = properties.SelectMany(x => x.Domain).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
                 edge.Range = properties.SelectMany(x => x.Range).Select(x => $"{Models.Wikidata.WikidataDump.EntityPrefix}{x}").ToList();
             }
 
             //TODO: Need some comments here. Which cases are this. Or some tests.
+            //TODO: Maybe more Unions are required.
             foreach (var node in graph.Nodes.Select(x => x.Value))
             {
-                if (node.GetInstanceOfValues(graph).Any())
+                if (node.IsGivenType)
                 {
-                    node.IsKnownType = true;
-                    node.Types = node.GetOutgoingNodes(graph).SelectMany(x => x.uris).Distinct().ToList();
-                }
-                else if (node.IsGivenType)
-                {
-                    node.IsKnownType = true;
                     node.Types = node.uris.ToList();
-                    //if (!string.IsNullOrWhiteSpace(entitiesIndexPath))
-                    //    node.Types = new BatchIdEntityQuery(entitiesIndexPath, node.uris.Distinct()).Query().SelectMany(x => x.InstanceOf).ToList();
+                }
+                else if (node.GetInstanceOfValues(graph).Any())
+                {
+                    node.Types = node.GetOutgoingNodes(graph).SelectMany(x => x.uris).Distinct().ToList();
+                    node.Types = new BatchIdEntityQuery(entitiesIndexPath, node.uris.Distinct()).Query().SelectMany(x => x.InstanceOf).ToList();
                 }
                 else
                 {
-                    if (node.GetOutgoingEdges(graph).Any(x => x.Domain.Any()))
+                    if (node.IsInferredTypeDomain)
                     {
-                        node.IsInferredTypeDomain = true;
                         node.InferredTypes = node.InferredTypes.Union(node.GetOutgoingEdges(graph).SelectMany(x => x.Domain)).ToList();
                     }
-                    if (node.GetIncommingEdges(graph).Any(x => x.Range.Any()))
+                    if (node.IsInferredTypeRange)
                     {
-                        node.IsInferredTypeRange = true;
-                        node.InferredTypes = node.InferredTypes.Union(node.GetIncommingEdges(graph).SelectMany(x => x.Range)).ToList();
+                        node.InferredTypes = node.InferredTypes.Union(node.GetIncomingEdges(graph).SelectMany(x => x.Range)).ToList();
                     }
                 }
             }
---
+++ SparqlForHumans.UnitTests/Query/QueryGraphExtensionTests.cs
@@ -0,0 +1,176 @@
+﻿using System.Linq;
+using SparqlForHumans.Lucene.Queries.Graph;
+using SparqlForHumans.Models.Query;
+using Xunit;
+
+namespace SparqlForHumans.UnitTests.Query
+{
+    public class QueryGraphExtensionTests
+    {
+        //var graph = new RDFExplorerGraph()
+        //{
+        //    nodes = new[]
+        //    {
+        //        new Node(0, "?var0", new[]{"http://www.wikidata.org/entity/Q76"} ),
+        //        new Node(1, "?var1"),
+        //    },
+        //    edges = new[]
+        //    {
+        //        new Edge(0, "?prop0", 0, 1)
+        //    },
+        //};
+        //var queryGraph = new QueryGraph(graph);
+        [Fact]
+        public void TestEdgeHasInstanceOfFalse()
+        {
+            var edge = new Edge(0, "?prop0", 0, 1);
+            var queryEdge = new QueryEdge(edge);
+            Assert.False(queryEdge.HasInstanceOf());
+        }
+
+        [Fact]
+        public void TestEdgeHasInstanceOfTrue()
+        {
+            var edge = new Edge(0, "?prop0", 0, 1, new[] { "http://www.wikidata.org/prop/direct/P31" });
+            var queryEdge = new QueryEdge(edge);
+            Assert.True(queryEdge.HasInstanceOf());
+        }
+
+        [Fact]
+        public void TestGetInstanceOfEdges()
+        {
+            var graph = new RDFExplorerGraph()
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var0" ),
+                    new Node(1, "?var1", new[]{"http://www.wikidata.org/entity/Q5"}),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?prop0", 0, 1, new []{"http://www.wikidata.org/prop/direct/P31"})
+                },
+            };
+            var queryGraph = new QueryGraph(graph);
+            var node0 = queryGraph.Nodes[0];
+            var instanceOfEdges0 = node0.GetInstanceOfEdges(queryGraph).ToArray();
+            Assert.Single(instanceOfEdges0);
+            Assert.Equal(0, instanceOfEdges0[0].id);
+
+            var node1 = queryGraph.Nodes[1];
+            var instanceOfEdges1 = node1.GetInstanceOfEdges(queryGraph).ToArray();
+            Assert.Empty(instanceOfEdges1);
+        }
+
+        [Fact]
+        public void TestGetEdgeSourceAndTargetNodes()
+        {
+            var graph = new RDFExplorerGraph()
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var0"),
+                    new Node(1, "?var1", new[]{"http://www.wikidata.org/entity/Q5"} ),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?prop0", 0, 1, new []{"http://www.wikidata.org/prop/direct/P31"})
+                },
+            };
+            var queryGraph = new QueryGraph(graph);
+            var node0 = queryGraph.Nodes[0];
+            var node1 = queryGraph.Nodes[1];
+            var edge0 = queryGraph.Edges[0];
+
+            Assert.Equal(node0, edge0.GetSourceNode(queryGraph));
+            Assert.Equal(node1, edge0.GetTargetNode(queryGraph));
+        }
+
+        [Fact]
+        public void TestGetNodeInstanceOfValues()
+        {
+            var graph = new RDFExplorerGraph()
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var0" ),
+                    new Node(1, "?var1", new[]{"http://www.wikidata.org/entity/Q5"}),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?prop0", 0, 1, new []{"http://www.wikidata.org/prop/direct/P31"})
+                },
+            };
+            var queryGraph = new QueryGraph(graph);
+            var node0 = queryGraph.Nodes[0];
+
+            Assert.Equal(new[] { "http://www.wikidata.org/entity/Q5" }, node0.GetInstanceOfValues(queryGraph));
+        }
+
+        [Fact]
+        public void TestGetNodeIncomingAndOutgoingEdges()
+        {
+            var graph = new RDFExplorerGraph()
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var0"),
+                    new Node(1, "?var1" ),
+                    new Node(2, "?var2" ),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?prop0", 0, 1),
+                    new Edge(1, "?prop0", 1, 2)
+                },
+            };
+            var queryGraph = new QueryGraph(graph);
+            var node0 = queryGraph.Nodes[0];
+            var node1 = queryGraph.Nodes[1];
+            var node2 = queryGraph.Nodes[2];
+            var edge0 = queryGraph.Edges[0];
+            var edge1 = queryGraph.Edges[1];
+
+            Assert.Single(node1.GetIncomingEdges(queryGraph));
+            Assert.Equal(edge0, node1.GetIncomingEdges(queryGraph).First());
+
+            Assert.Single(node1.GetOutgoingEdges(queryGraph));
+            Assert.Equal(edge1, node1.GetOutgoingEdges(queryGraph).First());
+
+            Assert.Single(node0.GetOutgoingEdges(queryGraph));
+            Assert.Equal(edge0, node0.GetOutgoingEdges(queryGraph).First());
+
+            Assert.Single(node2.GetIncomingEdges(queryGraph));
+            Assert.Equal(edge1, node2.GetIncomingEdges(queryGraph).First());
+        }
+
+        [Fact]
+        public void TestGetNodeIncomingAndOutgoingNodes()
+        {
+            var graph = new RDFExplorerGraph()
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var0"),
+                    new Node(1, "?var1" ),
+                    new Node(2, "?var2" ),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?prop0", 0, 1),
+                    new Edge(1, "?prop0", 1, 2)
+                },
+            };
+            var queryGraph = new QueryGraph(graph);
+            var node0 = queryGraph.Nodes[0];
+            var node1 = queryGraph.Nodes[1];
+            var node2 = queryGraph.Nodes[2];
+
+            Assert.Single(node1.GetIncomingNodes(queryGraph));
+            Assert.Equal(node0, node1.GetIncomingNodes(queryGraph).First());
+
+            Assert.Single(node1.GetOutgoingNodes(queryGraph));
+            Assert.Equal(node2, node1.GetOutgoingNodes(queryGraph).First());
+        }
+    }
+}
