b72ac9daa8d02fe0a3ede71270524581237968c2
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-11-11 21:56:39 +0100

    Add tests for the InMemory query engine

---
+++ SparqlForHumans.UnitTests/Query/InMemoryQueryEngineTests.cs
@@ -0,0 +1,58 @@
+﻿using SparqlForHumans.Lucene.Index;
+using SparqlForHumans.Lucene.Queries.Graph;
+using SparqlForHumans.Utilities;
+using Xunit;
+
+namespace SparqlForHumans.UnitTests.Query
+{
+    public class InMemoryQueryEngineTests
+    {
+        [Fact]
+        public void TestQueryDomain()
+        {
+            // Arrange
+            const string filename = @"Resources/QueryGraph.nt";
+            const string entitiesIndexPath = "QueryGraphEntities";
+            const string propertiesIndexPath = "QueryGraphProperties";
+            entitiesIndexPath.DeleteIfExists();
+            propertiesIndexPath.DeleteIfExists();
+            new EntitiesIndexer(filename, entitiesIndexPath).Index();
+            new PropertiesIndexer(filename, propertiesIndexPath).Index();
+
+            // Act
+            InMemoryQueryEngine.Init(entitiesIndexPath, propertiesIndexPath);
+            var actual = InMemoryQueryEngine.BatchIdPropertyDomainQuery(new[] {"http://www.wikidata.org/entity/Q5"});
+
+            Assert.Contains("http://www.wikidata.org/prop/direct/P25", actual);
+            Assert.Contains("http://www.wikidata.org/prop/direct/P27", actual);
+            Assert.Contains("http://www.wikidata.org/prop/direct/P777", actual);
+
+            // Cleanup
+            entitiesIndexPath.DeleteIfExists();
+            propertiesIndexPath.DeleteIfExists();
+        }
+
+        [Fact]
+        public void TestQueryRange()
+        {
+            // Arrange
+            const string filename = @"Resources/QueryGraph.nt";
+            const string entitiesIndexPath = "QueryGraphEntities";
+            const string propertiesIndexPath = "QueryGraphProperties";
+            entitiesIndexPath.DeleteIfExists();
+            propertiesIndexPath.DeleteIfExists();
+            new EntitiesIndexer(filename, entitiesIndexPath).Index();
+            new PropertiesIndexer(filename, propertiesIndexPath).Index();
+
+            // Act
+            InMemoryQueryEngine.Init(entitiesIndexPath, propertiesIndexPath);
+            var actual = InMemoryQueryEngine.BatchIdPropertyRangeQuery(new[] {"http://www.wikidata.org/entity/Q5"});
+
+            Assert.Contains("http://www.wikidata.org/prop/direct/P25", actual);
+
+            // Cleanup
+            entitiesIndexPath.DeleteIfExists();
+            propertiesIndexPath.DeleteIfExists();
+        }
+    }
+}
