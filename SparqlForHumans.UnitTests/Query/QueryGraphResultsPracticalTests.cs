e5e98ea7bd44694fb2ee94563397254bdeeecb37
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-12-08 16:17:51 +0100

    Adds practical test, built from wikidata samples

--- SparqlForHumans.UnitTests/Query/QueryGraphResultsPracticalTests.cs
+++ SparqlForHumans.UnitTests/Query/QueryGraphResultsPracticalTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using SparqlForHumans.Lucene;
 using SparqlForHumans.Lucene.Index;
 using SparqlForHumans.Lucene.Queries.Graph;
@@ -28,9 +25,20 @@ namespace SparqlForHumans.UnitTests.Query
             propertiesIndexPath.DeleteIfExists();
         }
 
-        //TODO: Create Sample for testing. Do not run on full Index;
+        /// <summary>
+        /// The following query has no results.
+        /// P180 should not be in the given results
+        /// PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+        ///    SELECT *
+        ///        WHERE {
+        ///        ?var2 wdt:P180 wd:Q6581072 .
+        ///        ?var2 wdt:P31 wd:Q5 .
+        ///        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+        ///}
+        ///LIMIT 100
+        /// </summary>
         [Fact]
-        public void TestResults_SampleFullIndex_OutgoingPropertiesOfKnownInstanceOfTypeShouldBeReducedInPossibilities()
+        public void TestResults_Practical_OutgoingPropertiesOfKnownInstanceOfTypeShouldBeReducedInPossibilities()
         {
             var graph = new RDFExplorerGraph
             {
@@ -48,14 +56,15 @@ namespace SparqlForHumans.UnitTests.Query
             };
 
             //Arrange:
-            var filename = "";
-            var entitiesIndexPath = "";
-            var propertiesIndexPath = "";
+            var filename = "Resources/QueryGraphPracticalResults1-Sorted.nt";
+            var entitiesIndexPath = "PracticalResultsEntitiesIndex";
+            var propertiesIndexPath = "PracticalResultsPropertiesIndex";
+
             CreateIndex(filename, entitiesIndexPath, propertiesIndexPath);
 
             // Act
             var queryGraph = new QueryGraph(graph);
-            queryGraph.GetGraphQueryResults(LuceneDirectoryDefaults.EntityIndexPath, LuceneDirectoryDefaults.PropertyIndexPath, false);
+            queryGraph.GetGraphQueryResults(entitiesIndexPath, propertiesIndexPath, false);
 
             var nodes = queryGraph.Nodes.Select(x=>x.Value).ToArray();
             var edges = queryGraph.Edges.Select(x=>x.Value).ToArray();
@@ -63,18 +72,54 @@ namespace SparqlForHumans.UnitTests.Query
             var actualResults = genderEdge.Results.Select(x => x.Label).ToList();
             Assert.Contains("sex or gender", actualResults);
 
-            Assert.DoesNotContain("has part", actualResults);
-            Assert.DoesNotContain("opposite of", actualResults);
-            Assert.DoesNotContain("is a list of", actualResults);
-
-            //Assert.DoesNotContain("different from", actualResults);
-            //Assert.DoesNotContain("field of work", actualResults);
+            Assert.DoesNotContain("depicts", actualResults);
 
             // Cleanup
             DeleteIndex(entitiesIndexPath, propertiesIndexPath);
         }
 
         //TODO: Create Sample for testing. Do not run on full Index;
+        [Fact]
+        public void TestResults_Practical_OutgoingPropertiesOfKnownOutgoingTypeShouldBeReducedInPossibilities()
+        {
+            var graph = new RDFExplorerGraph
+            {
+                nodes = new[]
+                {
+                    new Node(0, "?var1"),
+                    new Node(1, "?MAYOR", new[]{"http://www.wikidata.org/entity/Q30185"}),
+                    new Node(2, "?FEMALE",new[]{"http://www.wikidata.org/entity/Q6581072"}),
+                },
+                edges = new[]
+                {
+                    new Edge(0, "?POSITION_HELD", 0, 1, new[]{"http://www.wikidata.org/prop/direct/P39"}),
+                    new Edge(1, "?shouldBeGender", 0, 2),
+                }
+            };
+
+            //Arrange:
+            var filename = "Resources/QueryGraphPracticalResults1-Sorted.nt";
+            var entitiesIndexPath = "PracticalResultsEntitiesIndex";
+            var propertiesIndexPath = "PracticalResultsPropertiesIndex";
+
+            CreateIndex(filename, entitiesIndexPath, propertiesIndexPath);
+
+            // Act
+            var queryGraph = new QueryGraph(graph);
+            queryGraph.GetGraphQueryResults(entitiesIndexPath, propertiesIndexPath, false);
+
+            var nodes = queryGraph.Nodes.Select(x=>x.Value).ToArray();
+            var edges = queryGraph.Edges.Select(x=>x.Value).ToArray();
+            var genderEdge = edges[1];
+            var actualResults = genderEdge.Results.Select(x => x.Label).ToList();
+            Assert.Contains("sex or gender", actualResults);
+
+            Assert.DoesNotContain("depicts", actualResults);
+
+            // Cleanup
+            DeleteIndex(entitiesIndexPath, propertiesIndexPath);
+        }
+
         [Fact]
         public void TestResults_SampleFullIndex_OutgoingPropertiesOfKnownOutgoingTypeShouldBeReducedInPossibilities()
         {
@@ -93,6 +138,7 @@ namespace SparqlForHumans.UnitTests.Query
                 }
             };
 
+
             // Act
             var queryGraph = new QueryGraph(graph);
             queryGraph.GetGraphQueryResults(LuceneDirectoryDefaults.EntityIndexPath, LuceneDirectoryDefaults.PropertyIndexPath, false);
@@ -109,60 +155,45 @@ namespace SparqlForHumans.UnitTests.Query
 
             //Assert.DoesNotContain("different from", actualResults);
             //Assert.DoesNotContain("field of work", actualResults);
+
         }
         
-
-        /// <summary>
-        /// ?mother P25 ?son
-        /// ?mother ?prop ?var2
-        /// </summary>
         [Fact]
-        public void TestResults_IntersectDomain_3ConnectedNodes_IntersectDomains_3Nodes2Edge()
+        public void TestResults_SampleFullIndex_OutgoingPropertiesOfKnownInstanceOfTypeShouldBeReducedInPossibilities()
         {
             var graph = new RDFExplorerGraph
             {
                 nodes = new[]
                 {
-                    new Node(0, "?intersect"),
-                    new Node(1, "?COUNTRY", new[]{"http://www.wikidata.org/entity/Q6256"}),
-                    new Node(2, "?CITY", new[]{"http://www.wikidata.org/entity/Q515"}),
+                    new Node(0, "?var1"),
+                    new Node(1, "?HUMAN", new[]{"http://www.wikidata.org/entity/Q5"}),
+                    new Node(2, "?FEMALE",new[]{"http://www.wikidata.org/entity/Q6581072"}),
                 },
                 edges = new[]
                 {
-                    new Edge(0, "?prop1", 0, 1),
-                    new Edge(1, "?prop2", 0, 2),
+                    new Edge(0, "?instanceOf", 0, 1, new[]{"http://www.wikidata.org/prop/direct/P31"}),
+                    new Edge(1, "?shouldBeGender", 0, 2),
                 }
             };
 
-            // Arrange
+            // Act
             var queryGraph = new QueryGraph(graph);
-            
-            //Act
             queryGraph.GetGraphQueryResults(LuceneDirectoryDefaults.EntityIndexPath, LuceneDirectoryDefaults.PropertyIndexPath, false);
 
             var nodes = queryGraph.Nodes.Select(x=>x.Value).ToArray();
             var edges = queryGraph.Edges.Select(x=>x.Value).ToArray();
             var genderEdge = edges[1];
             var actualResults = genderEdge.Results.Select(x => x.Label).ToList();
+            Assert.Contains("sex or gender", actualResults);
 
-            //Assert
-            //Assert.Equal(2, queryGraph.Nodes[0].InferredBaseTypes.Count);
-            //Assert.Contains("Q5", queryGraph.Nodes[0].InferredBaseTypes);
+            Assert.DoesNotContain("has part", actualResults);
+            Assert.DoesNotContain("opposite of", actualResults);
+            Assert.DoesNotContain("is a list of", actualResults);
 
-            //Assert.Equal(5, queryGraph.Nodes[1].InferredBaseTypes.Count);
-            //Assert.Contains("Q5", queryGraph.Nodes[1].InferredBaseTypes);
-
-            //Assert.Empty(queryGraph.Nodes[2].InferredBaseTypes);
-
-            //Assert.Equal(2, queryGraph.Edges[0].DomainBaseTypes.Count);
-            //Assert.Contains("Q5", queryGraph.Edges[0].DomainBaseTypes);
-            //Assert.Equal(5, queryGraph.Edges[0].RangeBaseTypes.Count);
-            //Assert.Contains("Q5", queryGraph.Edges[0].RangeBaseTypes);
-
-            ////Since E1 source is HUMAN, Domain HUMAN
-            //Assert.Equal(2, queryGraph.Edges[1].DomainBaseTypes.Count);
-            //Assert.Contains("Q5", queryGraph.Edges[1].DomainBaseTypes);
-            //Assert.Empty(queryGraph.Edges[1].RangeBaseTypes);
+            //FAILS ON THESE:
+            Assert.DoesNotContain("different from", actualResults);
+            Assert.DoesNotContain("field of work", actualResults);
         }
+
     }
 }
