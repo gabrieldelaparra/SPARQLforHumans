3e90cf1552814ff99c0f27b2180ee9f09d9b2397
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2019-12-20 13:04:23 +0100

    Use IsReverseProperty instead.

--- SparqlForHumans.Lucene/Index/Fields/ReverseEntityPropertiesIndexer.cs
+++ SparqlForHumans.Lucene/Index/Fields/ReverseEntityPropertiesIndexer.cs
@@ -15,7 +15,7 @@ namespace SparqlForHumans.Lucene.Index.Fields
 
         public override bool FilterValidTriples(Triple triple)
         {
-            return triple.Predicate.GetPredicateType().Equals(PredicateType.ReverseProperty);
+            return triple.Predicate.IsReverseProperty();
         }
 
         public override IEnumerable<StringField> GetField(SubjectGroup tripleGroup)
