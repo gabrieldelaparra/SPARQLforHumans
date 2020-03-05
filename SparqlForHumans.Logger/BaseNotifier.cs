315614f50cb7cadb974a44e1f99052ec09a8f4cc
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-03-05 04:48:52 +0100

    Add a new notifier message, for debug/log

--- SparqlForHumans.Logger/BaseNotifier.cs
+++ SparqlForHumans.Logger/BaseNotifier.cs
@@ -4,7 +4,7 @@
     {
         private readonly NLog.Logger _logger = Logger.Init();
         public int NotifyTicks { get; set; } = 100000;
-        public abstract string NotifyMessage { get;  }
+        public abstract string NotifyMessage { get; }
 
         public virtual void LogProgress(long ticks, bool overrideCheck = false)
         {
@@ -17,6 +17,14 @@
         public virtual void LogException(long ticks, string message)
         {
             _logger.Error($"{message}, Count: {ticks:N0}");
+        }
+
+        public virtual void LogMessage(long ticks, string message, bool overrideTick = true)
+        {
+            if (ticks % NotifyTicks == 0 || overrideTick)
+            {
+                _logger.Info($"{message}, Count: {ticks:N0}");
+            }
         }
     }
 }
